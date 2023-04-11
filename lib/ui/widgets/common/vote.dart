c1b0609a1c571bfc843c4d2cc075488bd8caafc5
Datoa <fitonianaa@gmail.com>
2023-04-11 18:44:48 +0300

    librairy_UserInteractionProvider

--- lib/ui/widgets/common/vote.dart
+++ lib/ui/widgets/common/vote.dart
@@ -7,7 +7,7 @@ import 'package:provider/provider.dart';
 import 'package:rohy/domain/user/user.dart';
 import 'package:rohy/ui/providers/user_provider.dart';
 
-import '../providers/user_interaction_provider.dart';
+import '../../providers/user_interaction_provider.dart';
 
 class VoteWidget extends StatelessWidget {
 
